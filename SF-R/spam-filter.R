14146f1529c4cc041ebdd2be163772bcc322ee52
mohanenm <ethannmohan@gmail.com>
2018-03-16 10:08:24 -0500

    continue splitting

--- SF-R/spam-filter.R
+++ SF-R/spam-filter.R
@@ -15,8 +15,13 @@ names(email_data) <- sapply((1:nrow(names_data)), function(i) toString(names_dat
 # Changing y column to factor values, allows SVM classfication output
 email_data$y <- as.factor(email_data$y)
 #splliting data into a sample, to put lower load on SVM
-sample <- email_data[sample(nrow(email_data), 1000),]
-# Adding caret package so the SVM can actually be created
+thousand_sample <- email_data[sample(nrow(email_data), 1000),]
+# Adding packages so the SVM can actually be created
+# allows actual SVM to be used
 require(caret)
+#kernal based machine learning lab; adds methods for classification; deeper than caret package
 require(kernlab)
 require(doMC)
+# splliting test and train data; which rows will be for training and which rows will be for testing; 
+# keeps zero and one in both training and testing to guarantee homogenous training
+train <- creatDataPartition(thousand_sample$y, p=.85, list = FALSE, times = 1)
