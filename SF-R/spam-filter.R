be0ef33f2f0756047e7d06c123d89510265abd4a
mohanenm <ethannmohan@gmail.com>
2018-03-16 10:33:52 -0500

    formatting that probably does nothing, but may do something

--- SF-R/spam-filter.R
+++ SF-R/spam-filter.R
@@ -25,3 +25,27 @@ require(doMC)
 # splliting test and train data; which rows will be for training and which rows will be for testing; 
 # keeps zero and one in both training and testing to guarantee homogenous training
 train <- creatDataPartition(thousand_sample$y, p=.85, list = FALSE, times = 1)
+# finally (actual split)
+d_train <- thousand_sample[ train, ]
+d_test <- thousand_sample[-train, ]
+# domc
+# use all possible cores, very, very, very cool(increase SVM)
+registerDoMC(cores=4) # macbook air, 4 cores
+# finding the right tunning params for SVM is done by using the sigest function from kernlab
+# SVM needs sigma and c parameters
+if(FALSE){
+  "FROM 'A PRACTICAL GUIDE TO SVM CLASSIFICATION' ==> https://www.csie.ntu.edu.tw/~cjlin/papers/guide/guide.pdf
+  'We recommend a grid-search on C and γ using cross-validation. Various pairs of (C,γ) values are tried and the 
+  one with the best cross-validation accuracy is picked. We found that trying exponentially growing sequences of
+C and γ is a practical method to identify good parameters (for example, C=2−5,2−3,…,215;γ=2−15,2−13,…,23)"
+}
+if(FALSE){ "FROM ARTICLE THAT IS TEACHING ME HOW TO BUILD THIS: EASIER EXPLANATION THEN THE PREVIOUS ONE"
+  "If you want to know what these parameters are exactly you can take a look at:
+  http://feature-space.com/en/post65.html and http://stats.stackexchange.com/questions/31066/what-is-the-influence-of-c-in-svms-with-linear-kernel
+  The C parameter tells the SVM optimization how much you want to avoid misclassifying each training example. 
+  For large values of C, the optimization will choose a smaller-margin hyperplane if that hyperplane does a better
+  job of getting all the training points classified correctly. Conversely, a very small value of C will cause the 
+  optimizer to look for a larger-margin separating hyperplane, even if that hyperplane misclassifies more points. 
+  For very tiny values of C, you should get misclassified examples, often even if your training data is linearly 
+  separable."
+}
